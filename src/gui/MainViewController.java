4e44dc87e64c688c71aae9011e04667096aae957
crmpereira <cesar.pereiram@gmail.com>
2020-05-28 20:34:26 -0300

    Ajustes

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.DepartmentService;
 
 
 public class MainViewController implements Initializable {
@@ -36,7 +37,8 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-	
+		loadView("/gui/DepartmentList.fxml",x -> {});
+		
 	}
 	
 	@FXML
