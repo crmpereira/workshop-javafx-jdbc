dd42343d8f9a277f31ab932b8134eda01c4cef7c
crmpereira <cesar.pereiram@gmail.com>
2020-06-08 20:19:20 -0300

    Save Update

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -12,6 +12,16 @@ public class Utils {
 	}
 
 
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);	
+		}
+		catch(NumberFormatException e) {
+			return null; 
+		}
+		
+		
+	}
 	
 	
 
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -19,6 +19,17 @@ public class DepartmentService {
 		
 	}
 	
+	public void saveOrUpdate(Department obj) {
+		
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+		
+	}
+	
 	
 }
 
